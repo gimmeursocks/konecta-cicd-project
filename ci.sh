d480c42127875affbe2dd8b6e4a4528956c1c50e
gimmeursocks <bobteen1@gmail.com>
2025-09-18 15:37:04 +0300

    Add trivy image vulnerability scan and docker hub push

--- ci.sh
+++ ci.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 set -euo pipefail # Exit immediately if any command fails
 
+# load docker environment variables securely
+set -o allexport; source .env; set +o allexport
+
 echo "Starting CI process..."
 
 echo "Linting code with ESLint..."
-npx eslint .
+npx eslint . --max-warnings 0
 
 echo "Formatting code with Prettier..."
-npx prettier --write .
+npx prettier --check .
 
 echo "Running Tests with Jest..."
 npm test
 
-echo "Building and starting Docker containers..."
-docker compose up --build -d
+echo "Building Docker image..."
+docker compose build
+
+echo "Scanning image with Trivy..."
+# only show vulnerabilities if found
+trivy image \
+  --exit-code 1 \
+  --quiet \
+  --skip-version-check \
+  --ignore-unfixed \
+  --severity HIGH,CRITICAL \
+  --format json "${IMAGE_NAME}:${IMAGE_TAG}" |
+  jq '.Results[] | select(.Vulnerabilities | length > 0)'
+
+echo "Pushing image to Docker Hub..."
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+
+echo "Starting containers..."
+docker compose up -d
 
 echo "CI process completed successfully."
