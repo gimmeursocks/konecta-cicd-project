fce9e7b2e2b1fb89030bbe568ab1d4a2c023e772
gimmeursocks <bobteen1@gmail.com>
2025-09-24 19:50:08 +0300

    Fix versioning in s3

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -3,11 +3,6 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
